7d999be0dec0712619bbfbed314a2de7678628f7
shivhanchin07 <ravihanchin@gmail.com>
2025-04-24 11:27:22 +0530

    save file

---
+++ Demo.java
@@ -0,0 +1,10 @@
+ class Demo{
+    public static void main (String[]args){
+        int a=5;
+        int b=10;
+        int sum=a+b;
+        int sub=a-b;
+        System.out.println("The sum is: "+sum);
+        System.out.println("The sub is: "+sub);
+    }
+}
